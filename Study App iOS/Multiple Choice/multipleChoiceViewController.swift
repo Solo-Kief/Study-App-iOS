3884b8d47866d5d51bb8f519664b0f9a792758a7
Jake Dillon <jake.dillon168@gmail.com>
2018-11-27 14:20:41 -0600

    started on multiple choice

--- Study App iOS/Multiple Choice/multipleChoiceViewController.swift
+++ Study App iOS/Multiple Choice/multipleChoiceViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class multipleChoiceViewController: UIViewController {
     @IBOutlet var questionField: UITextView!
     @IBOutlet var questionFieldHeight: NSLayoutConstraint!
     @IBOutlet var answer1: UIButton!
@@ -26,13 +26,13 @@ class ViewController: UIViewController {
         
         guard let loadedQuestions = Question.loadArray() else {
             buildDefaultQuestions()
-            Question.saveArray(questions: ViewController.questions)
+            Question.saveArray(questions: multipleChoiceViewController.questions)
             loadQuestion()
             return
         }
-        ViewController.questions = loadedQuestions
+        multipleChoiceViewController.questions = loadedQuestions
         
-        if ViewController.questions.count == 0 { //temporary for testing.
+        if multipleChoiceViewController.questions.count == 0 { //temporary for testing.
             buildDefaultQuestions()
         }
         
@@ -40,18 +40,18 @@ class ViewController: UIViewController {
     }
 
     func loadQuestion() {
-        var selector = Int.random(in: 0..<ViewController.questions.count)
-        while lastQuestion == selector && ViewController.questions.count != 1 {
-            selector = Int.random(in: 0..<ViewController.questions.count)
+        var selector = Int.random(in: 0..<multipleChoiceViewController.questions.count)
+        while lastQuestion == selector && multipleChoiceViewController.questions.count != 1 {
+            selector = Int.random(in: 0..<multipleChoiceViewController.questions.count)
         } //Prevents a question from repeating back-to-back.
         lastQuestion = selector
         
-        questionField.text = ViewController.questions[selector].question
-        answer1.setTitle(ViewController.questions[selector].answers[0], for: .normal)
-        answer2.setTitle(ViewController.questions[selector].answers[1], for: .normal)
-        answer3.setTitle(ViewController.questions[selector].answers[2], for: .normal)
-        answer4.setTitle(ViewController.questions[selector].answers[3], for: .normal)
-        correctAnswer = ViewController.questions[selector].correctAnswer
+        questionField.text = multipleChoiceViewController.questions[selector].question
+        answer1.setTitle(multipleChoiceViewController.questions[selector].answers[0], for: .normal)
+        answer2.setTitle(multipleChoiceViewController.questions[selector].answers[1], for: .normal)
+        answer3.setTitle(multipleChoiceViewController.questions[selector].answers[2], for: .normal)
+        answer4.setTitle(multipleChoiceViewController.questions[selector].answers[3], for: .normal)
+        correctAnswer = multipleChoiceViewController.questions[selector].correctAnswer
         
         UIView.animate(withDuration: 0.25, animations: {
             self.questionField.backgroundColor = UIColor.clear
@@ -62,14 +62,14 @@ class ViewController: UIViewController {
     }
     
     func buildDefaultQuestions() { //temporary function for testing.
-        ViewController.questions.append(Question(Question: "How many buttons are on an iPhone X?", Answers: ["One", "Two", "Three", "Four"], CorrectAnswer: 3))
-        ViewController.questions.append(Question(Question: "But what of the poor gator?", Answers: ["Forever More", "Forever Blank", "Forever Great", "Fourever Pun"], CorrectAnswer: 2))
-        ViewController.questions.append(Question(Question: "Babe Ruth is assosiated with which sport?", Answers: ["Football", "Basketball", "Baseball", "Soccer"], CorrectAnswer: 3))
-        ViewController.questions.append(Question(Question: "What's the total number of dots on a pair of dice?", Answers: ["36", "32", "12", "42"], CorrectAnswer: 4))
-        ViewController.questions.append(Question(Question: "What planet is the closest to earth?", Answers: ["Venus", "Pluto", "Mars", "Neptune"], CorrectAnswer: 1))
-        ViewController.questions.append(Question(Question: "What is the tallest Mammal?", Answers: ["Elephant", "Human", "Giraffe", "Kangaroo"], CorrectAnswer: 3))
-        ViewController.questions.append(Question(Question: "How many strings does a violin have?", Answers: ["Three", "Four", "Five", "Six"], CorrectAnswer: 2))
-        ViewController.questions.append(Question(Question: "What is the chemical symbol for hydrogen?", Answers: ["Hy", "Hn", "HyGn", "H"], CorrectAnswer: 4))
+        multipleChoiceViewController.questions.append(Question(Question: "How many buttons are on an iPhone X?", Answers: ["One", "Two", "Three", "Four"], CorrectAnswer: 3))
+        multipleChoiceViewController.questions.append(Question(Question: "But what of the poor gator?", Answers: ["Forever More", "Forever Blank", "Forever Great", "Fourever Pun"], CorrectAnswer: 2))
+        multipleChoiceViewController.questions.append(Question(Question: "Babe Ruth is assosiated with which sport?", Answers: ["Football", "Basketball", "Baseball", "Soccer"], CorrectAnswer: 3))
+        multipleChoiceViewController.questions.append(Question(Question: "What's the total number of dots on a pair of dice?", Answers: ["36", "32", "12", "42"], CorrectAnswer: 4))
+        multipleChoiceViewController.questions.append(Question(Question: "What planet is the closest to earth?", Answers: ["Venus", "Pluto", "Mars", "Neptune"], CorrectAnswer: 1))
+        multipleChoiceViewController.questions.append(Question(Question: "What is the tallest Mammal?", Answers: ["Elephant", "Human", "Giraffe", "Kangaroo"], CorrectAnswer: 3))
+        multipleChoiceViewController.questions.append(Question(Question: "How many strings does a violin have?", Answers: ["Three", "Four", "Five", "Six"], CorrectAnswer: 2))
+        multipleChoiceViewController.questions.append(Question(Question: "What is the chemical symbol for hydrogen?", Answers: ["Hy", "Hn", "HyGn", "H"], CorrectAnswer: 4))
     }
     
     @IBAction func checkAnswer(_ sender: UIButton) {
