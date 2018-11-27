d09efbc54584ac63b8310772a0090727c35bbf8d
Brian Sadler <briansadler@MacBook-Pro-3.local>
2018-11-27 14:23:34 -0600

    created new branch and view controller and file

---
+++ Study App iOS/FlashCardViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FlashCardViewController.swift
+//  Study App iOS
+//
+//  Created by Brian Sadler on 11/27/18.
+//  Copyright © 2018 Phoenix Development. All rights reserved.
+//
+
+import UIKit
+//Brian's File
+
+
+class FlashCardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
