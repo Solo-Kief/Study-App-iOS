f5723e67468959f9f69cd1c801b9d86c29b6688f
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-11-28 14:33:28 -0600

    1. In FillInTheBlankViewController 	A. Created variable for dummyFillInTheBlankQuestionSet 	B. Removed unnecessary array of fillInTheBlankQuestions 	C. Created populateFillInTheBlankQuestions & getNewFillInTheBlankQuestion functions and set both functions to run in viewDidLoad function 	D. Created showCorrectAnswerAlert & showIncorrectAnswerAlert functions and set both functions to run in submitButtonTapped function

--- Study App iOS/Fill In The Blank/FillInTheBlankViewController.swift
+++ Study App iOS/Fill In The Blank/FillInTheBlankViewController.swift
@@ -13,6 +13,7 @@ class FillInTheBlankViewController: UIViewController {
     @IBOutlet weak var statementLabel: UILabel!
     @IBOutlet weak var answerTextField: UITextField!
     
+    var dummyFillInTheBlankQuestionSet: QuestionSet!
     
     //Current fill in the blank question being answered
     var currentFillInTheBlankQuestion: Question! {
@@ -21,9 +22,6 @@ class FillInTheBlankViewController: UIViewController {
             statementLabel.text = currentFillInTheBlankQuestion.question
         }
     }
-    
-    //Array of fill in the blank questions
-    var fillInTheBlankQuestions: [Question] = []
     
     //Array that holds the questions that have been answered
     var completedFillInTheBlankQuestions: [Question] = []
@@ -35,10 +33,68 @@ class FillInTheBlankViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        //INSERT FUNCTIONS HERE
+        populateFillInTheBlankQuestions()
+        getNewFillInTheBlankQuestion()
     }
     
+    //Popultates questions when the screen loads
+    func populateFillInTheBlankQuestions() {
+        //A Fill in the blank QuestionSet
+        let question1 = Question(Question: "In a deck of cards the king of ______ is the only king without a moustache.", Answers: ["Hearts"], CorrectAnswer: 0)
+        let question2 = Question(Question: "Stressed is ______ spelled backwards.", Answers: ["Desserts"], CorrectAnswer: 0)
+        let question3 = Question(Question: "The Twitter bird's actual name is ______.", Answers: ["Larry"], CorrectAnswer: 0)
+        dummyFillInTheBlankQuestionSet = QuestionSet(Title: "Dummy Fill In The Blank Questions", Details: nil, Questions: [question1, question2, question3], Style: .Blank)
+    }
     
+    //Gets a random question from the array of fill in the blank questions
+    func getNewFillInTheBlankQuestion() {
+        if dummyFillInTheBlankQuestionSet.questions.count > 0 {
+            //Get a random index from 0 to 1 less then the amount of elements in the questions array
+            randomIndex = Int(arc4random_uniform(UInt32(dummyFillInTheBlankQuestionSet.questions.count)))
+            //Set currentQuestion equal to the question that is at the random index in the questions array
+            currentFillInTheBlankQuestion = dummyFillInTheBlankQuestionSet.questions[randomIndex]
+        } else {
+            
+        }
+    }
+    
+    //Shows an alert when the user gets the question correct
+    func showCorrectAnswerAlert() {
+        //UIAlertController
+        let correctAlert = UIAlertController(title: "Correct", message: "\(currentFillInTheBlankQuestion.answers[0]) was the correct answer.", preferredStyle: .actionSheet)
+        //UIAlertAction
+        let closeAction = UIAlertAction(title: "Close", style: .default) { _ in
+            self.completedFillInTheBlankQuestions.append(self.dummyFillInTheBlankQuestionSet.questions.remove(at: self.randomIndex))
+            self.getNewFillInTheBlankQuestion()
+        }
+        //Add action to the alert controller
+        correctAlert.addAction(closeAction)
+        //Present the alert controller
+        self.present(correctAlert, animated: true, completion: nil)
+    }
+
+    //Shows an alert when the user get the question wrong
+    func showIncorrectAnswerAlert() {
+        //UIAlertController
+        let incorrectAlert = UIAlertController(title: "Incorrect", message: "\(currentFillInTheBlankQuestion.answers[0]) was the correct answer.", preferredStyle: .actionSheet)
+        //UIAlertAction
+        let closeAction = UIAlertAction(title: "Close", style: .default) { _ in
+            self.completedFillInTheBlankQuestions.append(self.dummyFillInTheBlankQuestionSet.questions.remove(at: self.randomIndex))
+            self.getNewFillInTheBlankQuestion()
+        }
+        //Add the action to the alert controller
+        incorrectAlert.addAction(closeAction)
+        //Present the alert controller
+        self.present(incorrectAlert, animated: true, completion: nil)
+    }
+
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        if answerTextField.text!.lowercased() == currentFillInTheBlankQuestion.answers[0] {
+            showCorrectAnswerAlert()
+        } else {
+            showIncorrectAnswerAlert()
+        }
+    }
     
 
     /*
@@ -51,4 +107,8 @@ class FillInTheBlankViewController: UIViewController {
     }
     */
 }
+
+
+
+//FIX FILL IN THE BLANK LINE
 
