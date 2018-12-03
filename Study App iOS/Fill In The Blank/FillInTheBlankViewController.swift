81db973e6ea5367ac56a23803bea00d98c21a733
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-12-03 08:54:16 -0600

    1. In FillInTheBlankViewController 	A. Added some comment reminders at the bottom of the screen

--- Study App iOS/Fill In The Blank/FillInTheBlankViewController.swift
+++ Study App iOS/Fill In The Blank/FillInTheBlankViewController.swift
@@ -117,3 +117,12 @@ class FillInTheBlankViewController: UIViewController {
      */
 }
 
+/*
+ 
+ IDEAS FOR FILL IN THE BLANK PAGE
+ 1. Fix placeholder stuff (Ex: Changing placeholder text to disapper when beginning to edit text in answerTextField)
+ 2. Change questionLabel.text make it show Correct or not correct with a sleep timer and then showing the next question? (Instead of having the pop up alert action thing)
+ 3. Set up a reset button?
+ 4. Set up a score keeper?
+
+*/
