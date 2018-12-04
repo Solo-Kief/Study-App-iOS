ae55e8476f7e51dee38ac2f3a618edf37e3e8865
Solomon Kieffer <solomonkeiffer@MacBook-Pro-80.local>
2018-12-04 10:06:34 -0600

    - Redid the constraints for the flash card screen. - Corrected an issue where the "next" button's corner radius was being overridden in then view did load.

--- Study App iOS/FlashCardViewController.swift
+++ Study App iOS/FlashCardViewController.swift
@@ -54,7 +54,6 @@ class FlashCardViewController: UIViewController {
     }
   
     override func viewDidAppear(_ animated: Bool) {
-        
         //Sets the colors for buttons and background
         self.view.backgroundColor = StorageEnclave.Access.getCurrentPrimaryColor()
         self.flipButton.titleLabel?.textColor = StorageEnclave.Access.getCurrentTextColor()
@@ -68,29 +67,8 @@ class FlashCardViewController: UIViewController {
         super.viewDidLoad()
         viewDidAppear(false)
         
-        
-        
-        nextButton.layer.cornerRadius = 10
-        
         //Makes the current card the very first card in the array
         currentCard = flashCardArray[0]
         flashCardTextView.text =  currentCard.question
-    
-        
-        
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
