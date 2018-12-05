464ffd78faaf1fb1d4cb00dc1e28d9884fabb67b
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-12-05 12:46:59 -0600

    1. Created AddEditQuestionSetViewController 2. Created AddEditQuestionsViewController 3. In Main.storyboard 	A. Set class for AddEditQuestionSet View Controller to AddEditQuestionSetViewController 	B. Set class for AddEditQuestions View Controller to AddEditQuestionsViewController

---
+++ Study App iOS/Add & Edit/AddEditQuestionSetViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddEditQuestionSetViewController.swift
+//  Study App iOS
+//
+//  Created by Matthew Riley on 12/5/18.
+//  Copyright © 2018 Phoenix Development. All rights reserved.
+//
+
+import UIKit
+
+class AddEditQuestionSetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
