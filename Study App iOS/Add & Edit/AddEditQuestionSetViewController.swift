eb6a101f6cc2994e6bf9db1c4b95d9c913e628a0
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-12-05 13:03:41 -0600

    1. Deleted and recreated AddEditQuestionSetViewController & AddEditQuestionsViewController 2. Reconnected AddEditQuestionSetViewController & AddEditQuestionsViewController to the appropriate view controllers

--- Study App iOS/Add & Edit/AddEditQuestionSetViewController.swift
+++ Study App iOS/Add & Edit/AddEditQuestionSetViewController.swift
@@ -9,13 +9,6 @@
 import UIKit
 
 class AddEditQuestionSetViewController: UIViewController {
-    
-    @IBOutlet weak var questionTextField: UITextField!
-    @IBOutlet weak var detailsTextField: UITextField!
-    @IBOutlet weak var questionSetTypeSegmentedControl: UISegmentedControl!
-    
-    
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,8 +26,5 @@ class AddEditQuestionSetViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    
-    
-    
 
 }
